48c4668719f5f5804fc8756ed6b1118c7e7fbb18
pedroAGullo <pedro.agullo.marco@gmail.com>
2021-05-25 10:14:20 +0200

    Conexion a la database funcionando.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
-// const db = require("./db.js");
 const app = express();
 const port = 3000;
+const db = require('./db.js');
 
 //Middleware
 app.use(express.json());
+app.use
 
-app.listen(port, () => console.log(`Node server running on http://localhost:${port}`));
 
-
+//Connecting to the database
+db
+.then(()=>{
+    //Starting server
+    app.listen(port, () => console.log(`Servidor levantado en http://localhost:${port}`));
+})
+.catch((err)=> console.log(err.message));
